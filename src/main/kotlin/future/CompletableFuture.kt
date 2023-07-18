21f1ef243841e1f2e14a634ad31c000c5d38dd62
NG <guraband@naver.com>
2023-07-18 23:54:25 +0900

    기본 프로젝트 생성 &  예제 파일 생성

---
+++ src/main/kotlin/future/CompletableFuture.kt
@@ -0,0 +1,24 @@
+package future
+
+import java.util.concurrent.CompletableFuture
+
+fun sum(n1: Int, n2: Int): Int {
+    return n1 + n2
+}
+
+fun main() {
+    val completableFutire = CompletableFuture.supplyAsync {
+        Thread.sleep(2_000)
+        sum(100, 200)
+    }
+
+    println("계산 시작")
+    completableFutire.thenApplyAsync(::println)
+
+    while (!completableFutire.isDone) {
+        Thread.sleep(500)
+        println("계산 결과를 집계 중입니다.")
+    }
+
+    println("계산 종료")
+}
